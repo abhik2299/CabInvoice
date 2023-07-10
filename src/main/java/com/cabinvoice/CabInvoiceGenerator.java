6fad2f52a11a3040c4316be55bdd8abb7d26c4ad
abhik2299 <abhishekgautam22999@gmail.com>
2023-07-10 13:07:04 +0530

    added

--- src/main/java/com/cabinvoice/CabInvoiceGenerator.java
+++ src/main/java/com/cabinvoice/CabInvoiceGenerator.java
@@ -1,5 +1,14 @@
 package com.cabinvoice;
 
 public class CabInvoiceGenerator {
-
+    public static final double costperkm =10.0;
+    public static final double costpermin =1.0;
+    public static final double minfare =5.0;
+    public double calculateFare(double distance,int time){
+        double totalfare=(distance*costperkm)+(time*costpermin);
+        if(totalfare<5) {
+            return minfare;
+        }
+        return totalfare;
+    }
 }
