c3e9dbbb1973a7de50e03ea5792bfdf1848e6a27
abhik2299 <abhishekgautam22999@gmail.com>
2023-07-10 13:16:17 +0530

    added

--- src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
@@ -1,16 +1,8 @@
 package com.cabtest;
 
-import com.cabinvoice.CabInvoiceGenerator;
 
 public class CabInvoiceGeneratorTest {
 
-        CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
-        @Test
-        public void TotalFare(){
-            double distance=2.0;
-            int time=10;
-            double res= cabInvoiceGenerator.calculateFare(distance,time);
-            A
 
     }
-}
+
