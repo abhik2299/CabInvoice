c75fdaa3eacda97465c58e62fc3eb32fcfbaf311
abhik2299 <abhishekgautam22999@gmail.com>
2023-07-10 12:57:13 +0530

    added

---
+++ src/main/java/com/cabinvoice/CabInvoiceGenerator.java
@@ -0,0 +1,5 @@
+package com.cabinvoice;
+
+public class CabInvoiceGenerator {
+
+}
---
+++ src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
@@ -0,0 +1,16 @@
+package com.cabtest;
+
+import com.cabinvoice.CabInvoiceGenerator;
+
+public class CabInvoiceGeneratorTest {
+
+        CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
+        @Test
+        public void TotalFare(){
+            double distance=2.0;
+            int time=10;
+            double res= cabInvoiceGenerator.calculateFare(distance,time);
+            A
+
+    }
+}
