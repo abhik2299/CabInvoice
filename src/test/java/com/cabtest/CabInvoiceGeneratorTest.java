cc46ea414705e6ebe8c1fa4a99af26d71a63388b
Atharva <atharva@Atharvas-MacBook-Air.local>
2023-07-10 13:28:17 +0530

    Test cases added

--- src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
@@ -1,8 +1,21 @@
 package com.cabtest;
 
 
+import com.cabinvoice.CabInvoiceGenerator;
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class CabInvoiceGeneratorTest {
+
+    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+    @Test
+    public void TotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double result = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25.0, result);
 
 
     }
-
+}
