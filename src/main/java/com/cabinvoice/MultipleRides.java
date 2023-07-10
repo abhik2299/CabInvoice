bdcf0abdc0d0be28797ec73fde568e71b9305881
abhik2299 <abhishekgautam22999@gmail.com>
2023-07-10 23:29:54 +0530

    added

---
+++ src/main/java/com/cabinvoice/MultipleRides.java
@@ -0,0 +1,4 @@
+package com.cabinvoice;
+
+public class MultipleRides {
+}
---
+++ src/test/java/com/cabtest/MultipleRidesTest.java
@@ -0,0 +1,13 @@
+package com.cabtest;
+
+import com.cabinvoice.MultipleRides;
+import org.junit.Test;
+
+public class MultipleRidesTest {
+    MultipleRides multipleRides=new MultipleRides();
+    @Test
+    public void testmultiplefares(){
+        double result=multipleRides.
+
+    }
+}
